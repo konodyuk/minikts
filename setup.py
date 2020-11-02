407d301e3f4132e44e92e36c452040dcbc47bf34
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-11-02 16:50:45 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="minikts",
+    version="0.0.1",
+    author="Nikita Konodyuk",
+    author_email="konodyuk@gmail.com",
+    description="Simple tool for running ML experiments",
+    url="https://github.com/konodyuk/minikts",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "rich",
+        "click",
+        "python-box",
+    ],
+    entry_points={
+        "console_scripts": ["minikts=minikts.cli:cli"]
+    },
+    include_package_data=True
+)
