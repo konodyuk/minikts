7820edf6b2b9b5d89d4053e29383e2a228247a33
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-11-03 10:33:52 +0300

    add docstring for patterns

--- minikts/logging/patterns.py
+++ minikts/logging/patterns.py
@@ -1,4 +1,24 @@
 class Patterns:
+    """Common patterns for stdout parsing
+
+    Examples:
+        >>> import minikts.api as kts
+        >>> from lightgbm import LGBMClassifier
+        >>> with kts.parse_stdout(kts.patterns.lightgbm, kts.MatplotlibCallback(interval=50)):
+        ...     model = LGBMClassifier(n_estimators=1000)
+        ...     model.fit(x_train, y_train, eval_set=[(x_train, y_train), (x_test, y_test)])
+        >>> with kts.parse_stdout(kts.patterns.lightgbm_valid_only, kts.MatplotlibCallback(interval=50)):
+        ...     model = LGBMClassifier(n_estimators=1000)
+        ...     model.fit(x_train, y_train, eval_set=[(x_test, y_test)])
+        >>> from catboost import CatBoostClassifier
+        >>> with kts.parse_stdout(kts.patterns.catboost, kts.MatplotlibCallback(interval=50)):
+        ...     model = CatBoostClassifier(n_estimators=1000)
+        ...     model.fit(x_train, y_train, eval_set=(x_test, y_test)])
+        >>> with kts.parse_stdout(kts.patterns.catboost, kts.MatplotlibCallback(interval=50)):
+        ...     model = CatBoostClassifier(n_estimators=1000)
+        ...     model.fit(x_train, y_train])
+    """
+
     catboost = ("learn: {train:f}", "test: {valid:f}", "{step:d}:")
     xgboost_valid_only = ("valid{}:{valid:f}", "[{step:d}]")
     lightgbm_valid_only = ("valid{}:{valid:f}", "[{step:d}]")
