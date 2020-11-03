083f96e95ca0df97d88c0c7c49878ded2301e5d1
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-11-03 11:47:30 +0300

    add lightfm patterns

--- minikts/logging/patterns.py
+++ minikts/logging/patterns.py
@@ -10,6 +10,7 @@ class Patterns:
         >>> with kts.parse_stdout(kts.patterns.lightgbm_valid_only, kts.MatplotlibCallback(interval=50)):
         ...     model = LGBMClassifier(n_estimators=1000)
         ...     model.fit(x_train, y_train, eval_set=[(x_test, y_test)])
+
         >>> from catboost import CatBoostClassifier
         >>> with kts.parse_stdout(kts.patterns.catboost, kts.MatplotlibCallback(interval=50)):
         ...     model = CatBoostClassifier(n_estimators=1000)
@@ -17,10 +18,16 @@ class Patterns:
         >>> with kts.parse_stdout(kts.patterns.catboost, kts.MatplotlibCallback(interval=50)):
         ...     model = CatBoostClassifier(n_estimators=1000)
         ...     model.fit(x_train, y_train])
+
+        >>> from lightfm import LightFM
+        >>> with kts.parse_stdout(kts.patterns.lightfm, kts.MatplotlibCallback(interval=2)):
+        ...     model = LightFM()
+        ...     model.fit(interactions, epochs=100, verbose=True)
     """
 
     catboost = ("learn: {train:f}", "test: {valid:f}", "{step:d}:")
     xgboost_valid_only = ("valid{}:{valid:f}", "[{step:d}]")
     lightgbm_valid_only = ("valid{}:{valid:f}", "[{step:d}]")
-    xgboost = ('valid{}0{}:{train:f}', 'valid{}1{}:{valid:f}', '[{step:d}]')
-    lightgbm = ('valid{}0{}:{train:f}', 'valid{}1{}:{valid:f}', '[{step:d}]')
+    xgboost = ("valid{}0{}:{train:f}", "valid{}1{}:{valid:f}", "[{step:d}]")
+    lightgbm = ("valid{}0{}:{train:f}", "valid{}1{}:{valid:f}", "[{step:d}]")
+    lightfm = ("{}{epoch:d}", "{}{step:d}")
