45ec10de6855680e439dc244471dfd148251af2b
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-11-06 17:24:59 +0300

    add utils

---
+++ minikts/utils.py
@@ -0,0 +1,34 @@
+import os
+import parse
+
+from minikts.config import config
+
+def find_next_of_format(fmt, paths=None, dir_path=None):
+    if dir_path is not None:
+        assert paths is None
+        paths = list(os.listdir(dir_path))
+    assert paths is not None
+    used_numbers = list()
+    for path in paths:
+        result = parse.search(fmt, path)
+        used_numbers.append(result.fixed[0])
+    used_numbers = sorted(used_numbers)
+    next_number = 0
+    while (next_number < len(used_numbers)
+           and next_number == used_numbers[next_number]):
+        next_number += 1
+    if dir_path is None:
+        return fmt.format(next_number)
+    return dir_path / fmt.format(next_number)
+
+def get_experiment_path(experiment_id):
+    return config.paths.experiments_dir / experiment_id
+
+def hash_dataframe(df):
+    raise NotImplementedError()
+
+def hash_source(function):
+    raise NotImplementedError()
+
+def _flatten_box(b):
+    return {key: b[key] for key in b.keys(dotted=True)}
