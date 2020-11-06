24d4a9bf0d1c1d0f5dd81ee820349d42cf6fb001
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-11-06 17:52:50 +0300

    debug x3 run_templates.sh

--- minikts/templates/run_templates.sh
+++ minikts/templates/run_templates.sh
@@ -2,8 +2,8 @@ mkdir ~/workdir
 mkdir ~/workdir/experiments
 mkdir ~/workdir/global_cache
 
-cd ~/work/minikts/minikts/templates/classification/catboost/
-bash run.sh
+cd ~/work/minikts/minikts/minikts/templates/classification/catboost/
+bash ./run.sh
 # bash regression/lightgbm/run.sh
 # bash recsys/lightfm/run.sh
 # bash recsys/implicit/run.sh
