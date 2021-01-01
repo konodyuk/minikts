a48a5abc54d9c8141a659ca9782ca9ffd5776a9f
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-01-01 14:47:42 +0300

    move neptune and tmux to extras

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 import setuptools
 
+extras = {
+    "neptune": ["neptune-client<=0.4.117", "PyJWT<=1.6.4"],
+    "tmux": ["libtmux"],
+}
+
+all_deps = []
+for group_name in extras:
+    all_deps += extras[group_name]
+extras["all"] = all_deps
+
 setuptools.setup(
     name="minikts",
     version="0.0.1",
@@ -8,6 +18,7 @@ setuptools.setup(
     description="Simple tool for running ML experiments",
     url="https://github.com/konodyuk/minikts",
     packages=setuptools.find_packages(),
+    extras_require=extras,
     install_requires=[
         "attrs",
         "click>=3.0",
@@ -16,8 +27,6 @@ setuptools.setup(
         "dill",
         "numpy",
         "pandas",
-        "neptune-client<=0.4.117",
-        "PyJWT<=1.6.4"
     ],
     entry_points={
         "console_scripts": ["minikts=minikts.cli:cli"]
