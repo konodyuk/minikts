1243a3d1a13ef5286b6a5e1a91ac3553df96973a
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-01-01 15:15:04 +0300

    logging.logger -> loggers.neptune

--- minikts/api.py
+++ minikts/api.py
@@ -5,6 +5,6 @@ from minikts.config import config, hparams, init, load_config
 from minikts.debug import debug
 from minikts.experiment import Experiment, endpoint, option
 from minikts.callbacks import *
-from minikts.logging.logger import logger
+from minikts.loggers.neptune import logger
 from minikts.parsing import parse_stdout, Patterns as patterns
 from minikts.profiler import profile, profiler
