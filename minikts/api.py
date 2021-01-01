c9a8d24911f0a5397357c2f0b92f8571990834b5
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-01-02 00:22:40 +0300

    reflect recent changes in minikts.api

--- minikts/api.py
+++ minikts/api.py
@@ -1,10 +1,11 @@
 import minikts.stl as stl
 from minikts.cache import (fast_global_cache, fast_local_cache, global_cache,
                            local_cache, process_cache)
-from minikts.config import config, hparams, init, load_config
+from minikts.callbacks import MatplotlibCallback, LoggerCallback
+from minikts.cli import CLI, config_option
+from minikts.config import config, hparams, load_config
+from minikts.context import context, ctx, init
 from minikts.debug import debug
-from minikts.experiment import Experiment, endpoint, option
-from minikts.callbacks import *
-from minikts.loggers.neptune import logger
+from minikts.loggers import NeptuneLogger
 from minikts.parsing import parse_stdout, Patterns as patterns
 from minikts.profiler import profile, profiler
