3ed0b89091c0b3f45de37707c890e71e41b7a3c7
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-01-01 14:56:48 +0300

    implement CLI class

---
+++ minikts/cli.py
@@ -0,0 +1,58 @@
+import abc
+import click
+import functools
+
+class CLIMeta(abc.ABCMeta):
+    def __new__(cls, name, bases, members):
+        click_params = members["__click_params__"] = dict()
+        for base in bases:
+            if hasattr(base, "__click_params__"):
+                click_params.update(base.__click_params__)
+        for name, member in members.items():
+            if hasattr(member, "__click_params__"):
+                click_params[name] = member.__click_params__
+        return abc.ABCMeta.__new__(cls, name, bases, members)
+    
+def wrap(method, click_params):
+    @functools.wraps(method)
+    def wrapped(*args, **kwargs):
+        return method(*args, **kwargs)
+    setattr(wrapped, "__click_params__", click_params)
+    return wrapped
+
+class CLI(metaclass=CLIMeta):
+    """Turns all methods that have click options into CLI endpoints
+
+    Examples:
+        >>> class Experiment(CLI):
+        ...     @click.option(...)
+        ...     def train(...):
+        ...         ...
+        ...     @click.option(...)
+        ...     def predict(...):
+        ...         ...
+        >>> if __name__ == "__main__":
+        ...     exp = Experiment()
+        ...     exp.run()
+
+        >>> class Runner(CLI):
+        ...     @click.option(...)
+        ...     def submit(...):
+        ...         ...
+        ...     @click.option(...)
+        ...     def blend(...):
+        ...         ...
+        >>> if __name__ == "__main__":
+        ...     Runner().run()
+    """
+    def run(self, **kwargs):
+        @click.group(chain=True)
+        def _cli():
+            pass
+        
+        for name, params in self.__click_params__.items():
+            method = getattr(self, name)
+            wrapped = wrap(method, params)
+            _cli.command(name)(wrapped)
+            
+        _cli(standalone_mode=False, **kwargs)
